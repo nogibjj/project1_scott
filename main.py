5da81d400430569d2d03945b41a93f72bdd36050
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-02-07 23:49:22 -0500

    edit main.py and script.js

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import json
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+import openai
+import os
 
 app = FastAPI()
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1:8000/",
+    "http://127.0.0.1:5500/",
+]
+
+# Allow requests from all domains
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,5 +26,11 @@ app.add_middleware(
 )
 
 @app.post("/")
-async def convert_to_lowercase(request: Request, input: str = None):
-    return JSONResponse({"output": input.lower()})
+async def root(request: Request):
+    data = await request.json()
+    input_text = data["input"]
+    response = openai.Completion.create(
+     model = "GPT-3 interface",
+     prompt = input_text,   
+    )
+    return JSONResponse(content={"input": response['choices'][0]['text']})
--- script.js
+++ script.js
@@ -1,33 +1,30 @@
+// Get form and input elements
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+
+// Handle form submit event
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Get user input value
   const userInput = input.value;
-  const newInput = document.createElement("p");
-  newInput.innerText = userInput;
-  form.appendChild(newInput);
-  input.value = "";
+
+  // Send user input to FastAPI endpoint
+  fetch("http://127.0.0.1:8000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ input: userInput }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      // add the data to the html after the button as <p> tags
+      const p = document.createElement("p");
+      p.innerText = data.input;
+      form.appendChild(p);
+      // Do something with the response data
+    });
 });
 
-// document.getElementById("form").addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   const input = document.getElementById("input").value;
-
-//   fetch("http://localhost:8000/", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       "input": input
-//     })
-//   })
-//     .then(response => response.json())
-//     .then(data => {
-//       const p = document.createElement("p");
-//       p.innerHTML = `Output: ${data.output}`;
-//       p.innerText = data.input;
-//       form.appendChild(p);
-//       document.getElementById("form").appendChild(p);
-//     });
-// });
